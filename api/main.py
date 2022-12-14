603f1d88690739b09ed0c650ae09c808286633f2
oso1248 <oso1248@gmail.com>
2022-12-14 15:40:27 -0700

    updated readme

--- api/main.py
+++ api/main.py
@@ -5,8 +5,6 @@ from fastapi import FastAPI
 from loguru import logger
 import logging
 import sys
-# from fastapi.staticfiles import StaticFiles
-# from fastapi.openapi.docs import get_swagger_ui_html, get_swagger_ui_oauth2_redirect_html
 
 
 uvicorn_error = logging.getLogger("uvicorn.error")
@@ -38,22 +36,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-# @app.get("/docs", include_in_schema=False)
-# async def custom_swagger_ui_html():
-#     return get_swagger_ui_html(
-#         openapi_url=app.openapi_url,
-#         title=app.title + " - Swagger UI",
-#         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-#         swagger_js_url="/static/swagger-ui-bundle.js",
-#         swagger_css_url="/static/swagger-ui.css",
-#     )
-#
-#
-# @app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
-# async def swagger_ui_redirect():
-#     return get_swagger_ui_oauth2_redirect_html()
 
 
 @app.get("/", tags=['Root'], include_in_schema=False)
