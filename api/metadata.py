f140029a918ddbf963f272d3ba9ad21b3b9b6c38
oso1248 <oso1248@gmail.com>
2022-12-27 12:53:57 -0700

    updated metadata

--- api/metadata.py
+++ api/metadata.py
@@ -1,30 +1,30 @@
 description = f"""\
-*[Setup Instructions](https://github.com/oso1248/baseAPI/blob/master/README.md)*<br><br>
+*[Setup Instructions](https://github.com/oso1248/db_material/blob/master/README.md)*<br><br>
 """
 
 
 title_metadata = "FTC Material Database   (͡• ͜ʖ ͡•) 》🇺🇲"
 version_metadata = "0.0.1"
-terms_metadata = "https://github.com/oso1248/baseAPI/blob/master/LICENSE"
+terms_metadata = "https://github.com/oso1248/db_material/blob/master/LICENSE"
 contact_metadata = {"name": "Adam Coulson", "url": "https://github.com/oso1248", "email": "oso1248@gmail.com"}
-licence_metadata = {"name": "MIT License Copyright © 2022 Adam Coulson", "url": "https://github.com/oso1248/baseAPI/blob/master/LICENSE"}
+licence_metadata = {"name": "MIT License Copyright © 2022 Adam Coulson", "url": "https://github.com/oso1248/db_material/blob/master/LICENSE"}
 ui_metadata = {"deepLinking": True, "showExtensions": True, "showCommonExtensions": True, "docExpansion": "none", "syntaxHighlight": True, "syntaxHighlight.theme": "obsidian"}
 
 
 tags_metadata = [
-    {"name": "Auth", "description": "Authentication Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/AUTH.md"}},
-    {"name": "Users", "description": "Users Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Brands Brewing", "description": "Brewing Brands Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Brands Finishing", "description": "Finishing Brands Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Brands Packaging", "description": "Packaging Brands Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Suppliers", "description": "Supplier Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Commodity", "description": "Commodity Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Inventory Dates", "description": "Inventory Dates Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Inventory Material", "description": "Inventory Material Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Inventory Hop", "description": "Inventory Hop Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Inventory Last Brews", "description": "Inventory Last Brews Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Inventory Hibernate", "description": "Inventory Hibernate Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Brewing Addition", "description": "Brewing Addition Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Brewing Kettle Hop", "description": "Brewing Kettle Hop Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
-    {"name": "Brewing Dry Hop", "description": "Brewing Dry Hop Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/baseAPI/blob/master/api/routers/README/USERS.md"}},
+    {"name": "Auth", "description": "Authentication Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/AUTH.md"}},
+    {"name": "Users", "description": "Users Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/USERS.md"}},
+    {"name": "Brands Brewing", "description": "Brewing Brands Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/BRANDS.md"}},
+    {"name": "Brands Finishing", "description": "Finishing Brands Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/BRANDS.md"}},
+    {"name": "Brands Packaging", "description": "Packaging Brands Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/BRANDS.md"}},
+    {"name": "Suppliers", "description": "Supplier Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/SUPPLIERS.md"}},
+    {"name": "Commodity", "description": "Commodity Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/COMMODITY.md"}},
+    {"name": "Inventory Dates", "description": "Inventory Dates Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/INVENTORY.md"}},
+    {"name": "Inventory Material", "description": "Inventory Material Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/INVENTORY.md"}},
+    {"name": "Inventory Hop", "description": "Inventory Hop Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/INVENTORY.md"}},
+    {"name": "Inventory Last Brews", "description": "Inventory Last Brews Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/INVENTORY.md"}},
+    {"name": "Inventory Hibernate", "description": "Inventory Hibernate Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/INVENTORY.md"}},
+    {"name": "Brewing Addition", "description": "Brewing Addition Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/BRIDGESBREWING.md"}},
+    {"name": "Brewing Kettle Hop", "description": "Brewing Kettle Hop Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/BRIDGESBREWING.md"}},
+    {"name": "Brewing Dry Hop", "description": "Brewing Dry Hop Endpoints", "externalDocs": {"description": "¿Would You Like To Know More?", "url": "https://github.com/oso1248/db_material/blob/master/api/routers/README/BRIDGESBREWING.md"}},
 ]
